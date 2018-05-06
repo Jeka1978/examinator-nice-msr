70bce7bc58de20554c4cf49f3f6fed2b5e155e56
bsevgeny@gmail.com <papakita2009>
2018-05-06 16:15:42 +0300

    temp final version

--- src/main/java/com/nice/exams/examinator/ExaminatorApplication.java
+++ src/main/java/com/nice/exams/examinator/ExaminatorApplication.java
@@ -2,9 +2,21 @@ package com.nice.exams.examinator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ExaminatorApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExaminatorApplication.class, args);
--- src/main/java/com/nice/exams/examinator/controllers/ExamController.java
+++ src/main/java/com/nice/exams/examinator/controllers/ExamController.java
@@ -1,16 +1,20 @@
 package com.nice.exams.examinator.controllers;
 
 import com.nice.exams.examinator.CheckedExam;
-import com.nice.exams.examinator.model.ExamRequest;
 import com.nice.exams.examinator.model.Exercise;
 import com.nice.exams.examinator.model.SolvedExam;
 import com.nice.exams.examinator.services.ExamService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Evgeny Borisov
@@ -21,13 +25,21 @@ import java.util.Map;
 public class ExamController {
 
     private final ExamService examService;
+    private final RestTemplate restTemplate;
 
 
     @PostMapping("/exam")
     public List<Exercise> getExam(@RequestBody Map<String,Integer> examRequest) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.
-        return null;
+
+        List exercises = new ArrayList<>();
+        for (String microserviceName : examRequest.keySet()) {
+            Integer amount = examRequest.get(microserviceName);
+            microserviceName += "-exam";
+            exercises.add(restTemplate.getForObject("http://" + microserviceName + "/java/random?amount=" + amount, List.class));
+        }
+
+
+        return exercises;
 
     }
 
